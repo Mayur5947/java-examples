0a1267e8ab27919ae72fe83c64cc3b48dc47bd62
Mayur5947 <varmamayur37@gmail.com>
2024-01-09 08:47:47 +0530

    OOP'S programs

---
+++ stl_oop's_programs/src/com/stl/inheritance/Company.java
@@ -0,0 +1,12 @@
+package com.stl.inheritance;
+
+public class Company {
+
+	public void getCompanyName() {
+		System.out.println("Company name is Infosys");
+	}
+	
+	public void getCompanyLocation() {
+		System.out.println("location is pune");
+	}
+}
